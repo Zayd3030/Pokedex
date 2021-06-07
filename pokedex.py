4bd39f88635f144bb1ec5bf3c669fe92bc7b1b11
Zayd Hussain <76216005+Zayd3030@users.noreply.github.com>
2021-06-07 21:17:25 +0100

    Add Pokedex

---
+++ pokedex.py
@@ -0,0 +1,60 @@
+#Import Modules
+import pypokedex
+import PIL.Image, PIL.ImageTk
+import tkinter as tk
+from pypokedex import pokemon
+import urllib3
+from io import BytesIO
+
+from urllib3 import response
+
+#Creates Window
+window = tk.Tk()
+window.geometry("600x500")
+window.title("Zayd3030 Python Pokedex")
+window.config(padx=10, pady=10)
+
+#Creates Title
+title_label = tk.Label(window, text="Zayd3030 Pokedex")
+title_label.config(font=("Arial", 32))
+title_label.pack(padx=10, pady=10)
+
+pokemon_image = tk.Label(window)
+pokemon_image.pack(padx=10, pady=10)
+
+pokemon_info = tk.Label(window)
+pokemon_info.config(font=("Arial", 20))
+pokemon_info.pack(padx=10, pady=10)
+
+pokemon_types = tk.Label(window)
+pokemon_types.config(font=("Arial", 20))
+pokemon_types.pack(padx=10, pady=10)
+
+#Gets Pokemon from database
+def load_pokemon():
+    pokemon = pypokedex.get(name=text_id_name.get(1.0, "end-1c"))
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', pokemon.sprites.front.get('default'))
+    image = PIL.Image.open(BytesIO(response.data))
+    
+    img = PIL.ImageTk.PhotoImage(image)
+    pokemon_image.config(image=img)
+    pokemon_image.image = img
+
+    pokemon_info.config(text=f"{pokemon.dex} - {pokemon.name}".title())
+    pokemon_types.config(text=" - ".join([t for t in pokemon.types]).title())
+
+label_id_name = tk.Label(window, text="Pokemon ID or Name:")
+label_id_name.config(font=("Arial", 20))
+label_id_name.pack(padx=10, pady=10)
+
+text_id_name = tk.Text(window, height=1)
+text_id_name.config(font=("Arial", 20))
+text_id_name.pack(padx=10, pady=10)
+
+btn_load = tk.Button(window, text="Load Pokemon", command=load_pokemon)
+btn_load.config(font=("Arial", 20))
+btn_load.pack(padx=10, pady=10)
+
+window.mainloop()
